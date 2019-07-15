12149752e463116eec21ad9854aa2f3714126b87
Sofyanlabs <a.sofyann27@gmail.com>
2019-07-15 18:02:01 +0700

    JWT

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,17 @@ Route::apiResource('/status/{status}/reply','ReplyController');
 
 Route::post('/like/{reply}', 'LikeController@LikeIt');
 Route::delete('/like/{reply}', 'LikeController@unLikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
