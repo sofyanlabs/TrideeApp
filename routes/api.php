915b75cbe8976050ff1d9fb00f0522000207f251
Sofyanlabs <a.sofyann27@gmail.com>
2019-07-15 17:02:45 +0700

    Likes

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ Route::apiResource('/status', 'StatusController');
 Route::apiResource('/category', 'CategoryController');
 
 Route::apiResource('/status/{status}/reply','ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@LikeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
